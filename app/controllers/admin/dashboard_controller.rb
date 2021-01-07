5fe9ec658ccad898c406f5bf6270bb60e077d3af
Samantha Fok <samantha.fok@hotmail.com>
2021-01-07 07:28:49 +0000

    Updated admin dashboard to include count of products and categories in database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,15 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
-  password: ENV["HTTP_BASIC_PASSWORD"] 
-  if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+  password: ENV["HTTP_BASIC_PASSWORD"]
   
+  def index
+    @products = Product.order(id: :desc).all
+  end
+
   def show
+    @products = Product.all
+    @product = Product.find params[:id]
+    @count = Product.count
+
   end
 end

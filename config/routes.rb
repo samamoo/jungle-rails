10095a8dc5624c45c7add8a13966f9bb07a0f561
Samantha Fok <samantha.fok@hotmail.com>
2021-01-08 06:43:55 +0000

    Added route for users sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories
+    resources :categories, only: [:index, :create, :new]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

d265c32a1bd143394942fefca85d56a4c5b7253e
Samantha Fok <samantha.fok@hotmail.com>
2021-01-11 17:17:40 +0000

    Created User Authentication and verification for new users and existing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Account created successfully'
     else
       redirect_to '/signup'
     end
   end
 
   private
-
+  
   def user_params
     params.require(:user).permit(
       :first_name,
-      last_name,
+      :last_name,
       :email, 
       :password, 
       :password_confirmation)

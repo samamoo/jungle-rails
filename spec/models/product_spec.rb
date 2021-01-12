adfd4557aa93b74fad524c1e8dfcaedb9768cfe0
Samantha Fok <samantha.fok@hotmail.com>
2021-01-12 22:53:38 +0000

    Modified tests to test for error messages incase the validations doesn't pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -32,35 +32,39 @@ RSpec.describe Product, type: :model do
     # PRICE
     it 'should validate the price is present' do
       @category = Category.create! name: 'Shoes'
-      @product = @category.products.create!(
+      @product = @category.products.create(
         name: 'Scarpa Instincts',
         description: ' aggressive shoes',
-        price: 300,
         image:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSYfhzyWPPtBGI9HhMIRItB87z7lj4DpI6HIlH1wA09xgVQ_S3cp17MZ6II17cf_fJKIdHphG8dVDiZ&usqp=CAc',
         quantity: 4
       )
+      @product.errors.full_messages.each do |message|
+        puts message
+      end
 
-      expect(@product.price).to be_nil
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     # QUANTITY
     it 'should validate the quantity is present' do
       @category = Category.create! name: 'Shoes'
-      @product = @category.products.create!(
+      @product = @category.products.create(
         name: 'Scarpa Instincts',
         description: ' aggressive shoes',
         price: 300,
         image:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSYfhzyWPPtBGI9HhMIRItB87z7lj4DpI6HIlH1wA09xgVQ_S3cp17MZ6II17cf_fJKIdHphG8dVDiZ&usqp=CAc',
-        quantity: 4
       )
+      @product.errors.full_messages.each do |message|
+        puts message
+      end
 
-      expect(@product.quantity).to be_nil
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     # NAME - checking error messages
     it 'should validate the name is present' do
       @category = Category.create! name: 'Shoes'
-      @product = @category.products.create!(
+      @product = @category.products.create(
         name: '',
         description: ' aggressive shoes',
         price: 300,
@@ -71,13 +75,13 @@ RSpec.describe Product, type: :model do
         puts message
       end
 
-      expect(@product.name).to be_present
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     # CATEGORY ID
     it 'should validate the category id is present' do
       @category = Category.create! name: 'Shoes'
-      @product = @category.products.create!(
+      @product = @category.products.create(
         name: 'Scarpa Instincts',
         description: ' aggressive shoes',
         price: 300,
@@ -85,7 +89,7 @@ RSpec.describe Product, type: :model do
         quantity: 4
       )
 
-      expect(@product.category_id).to be_nil
+      expect(@product.category_id).to be_a(Integer)
     end
 
   end
